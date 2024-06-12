43153347608f08b836fce4f06d4fdb8fb28b8c53
Legionman69 <legionman.xbox@gmail.com>
2024-06-13 00:56:35 +0200

    first commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from .comfy_terminal import TerminalNode
+
+# A dictionary that contains all nodes you want to export with their names
+NODE_CLASS_MAPPINGS = {
+    "TerminalNode": TerminalNode
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TerminalNode": "Terminal Node"
+}
+
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
