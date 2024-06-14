e0b74bee6f58e55e9f04da1a57abf7caa8ec47d3
Legionman69 <legionman.xbox@gmail.com>
2024-06-14 08:49:00 +0200

    dfsdf

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from .comfy_terminal import TerminalNode
+from .comfy_terminal_display import TerminalDisplayNode
 
 # A dictionary that contains all nodes you want to export with their names
 NODE_CLASS_MAPPINGS = {     
-    "TerminalNode": TerminalNode
+    "TerminalNode": TerminalNode,
+    "TerminalDisplayNode": TerminalDisplayNode
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TerminalNode": "Terminal Node"
+    "TerminalNode": "Terminal Node",
+    "TerminalDisplayNode": "Terminal Display Node"
 }
 
 # Export symbols
