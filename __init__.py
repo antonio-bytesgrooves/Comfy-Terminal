2ee50083e19c65f12fabe963e50b8aa07f99a6c7
Legionman69 <legionman.xbox@gmail.com>
2024-06-14 11:07:15 +0200

    dfds

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .comfy_terminal_display import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .comfy_terminal import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 # Export symbols
 __all__ = {['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']}
